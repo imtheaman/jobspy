5864d29296a961a8c2aef7224c8d89a8347bdedc
Aman Kumar <imtheaman@proton.me>
2025-05-06 01:17:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import csv
 from jobspy import scrape_jobs
 
 jobs = scrape_jobs(
-    site_name=["indeed", "linkedin", "glassdoor", "google", "naukri"],
+    site_name=["indeed", "linkedin", "glassdoor", "naukri"],
     search_term="senior react native developer",
     google_search_term="senior react native developer jobs in Bangalore",
     location="Bangalore, India",
     results_wanted=250,
-    hours_old=7 * 24,
+    hours_old=72,
     country_indeed='india',
-    
-    # linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
+    linkedin_fetch_description=True # gets more info such as description, direct job url (slower)
     # proxies=["208.195.175.46:65095", "208.195.175.45:65095", "localhost"],
 )
 print(f"Found {len(jobs)} jobs")
